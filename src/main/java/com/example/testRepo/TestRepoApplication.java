f5ca573694b38e22db98ef7664b149c0be5f98f2
Dharani <kumar.skm03@gmail.com>
2022-12-24 15:32:50 +0530

    first commit

---
+++ src/main/java/com/example/testRepo/TestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRepoApplication.class, args);
+	}
+
+}
